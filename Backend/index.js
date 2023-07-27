4bc2d6326352f38f938ebbf8c3e3581f0c02c741
Charankasa07 <s180067@rguktsklm.ac.in>
2023-07-27 21:54:40 +0530

    27-07-2023

--- Backend/index.js
+++ Backend/index.js
@@ -18,6 +18,16 @@ app.use(cors({
   methods:["POST","GET"],
   credentials:true
 }))
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json())
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
