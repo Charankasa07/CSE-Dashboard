673bbe786f1a27440e8e2684d0fa6dbec5eac252
Charankasa07 <s180067@rguktsklm.ac.in>
2023-07-28 14:19:57 +0530

    28-07-2023

--- Backend/index.js
+++ Backend/index.js
@@ -20,13 +20,21 @@ const student_schema = require("./models/student");
 // }))
 
 app.use((req, res, next) => {
-  req.setHeader(`Access-Control-Allow-Origin : https://rguktsklmplacements.web.app`);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+//   req.setHeader(`Access-Control-Allow-Origin : https://rguktsklmplacements.web.app`);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://rguktsklmplacements.web.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
-});
+  });
 
 app.use(express.json())
 app.use(bodyparser.json());
